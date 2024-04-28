9059735b149405be6dd45536b4f19a7644a1bfa2
NadavElbaz <147098859+NadavElbaz@users.noreply.github.com>
2024-04-28 13:06:55 +0300

    Add files via upload

---
+++ Exercises.sql
@@ -0,0 +1,88 @@
+---Exercise Number 1
+
+/*
+Return the first name of the employee and the number of times this name appears in the table
+*/
+
+---Exercise Number 2
+
+/*
+Write a query that displays the first name of the employee that is repeated more than once in the table
+Working
+*/
+
+--- Exercise Number 3
+
+/*
+ Write a query that displays the first name, last name, and job start date of
+the employees from marketing and service departments who started their work before 2015 and arrange your
+ presentation by the employees from the most senior employee to the least senior employee
+*/
+
+--- Exercise Number 4
+
+/*
+Write a query that shows the name of the products whose price is between NIS 250 and NIS 600
+*/
+
+--- Exercise Number 5
+
+/*
+Write a query that displays the first name and last name of all employees
+whose salary is over NIS 20,000 or who sold a mouse
+*/
+
+--- Exercise Number 6
+
+/*
+From the employee table, display the smallest last name alphabetically
+who sold a keyboard
+*/
+
+--- Exercise Number 7
+
+/*
+Write a query showing for each product whether it is cheap or expensive according to the following rule:
+'Expensive' = the price of the product is greater than or equal to 500 NIS
+'Cheap' = everything else
+*/
+
+--- Exercise Number 8
+
+/*
+Write a query based on the previous section in which the amount of sales for each employee in the division will be displayed
+cheap/expensive.
+*/
+
+--- Exercise Number 9
+
+/*
+Display from the employee table the first name, last name and salary of each
+Employees who work in the unit of employee number '123'
+*/
+
+--- Exercise Number 10
+
+/*
+Display the first name, last name and new email address from the employee table
+that you will produce.
+The employee's email address will consist of the first letter of their first name
+Concatenated with the first 2 letters of their last name and concatenated with the employee number, with the suffix of:
+"computer.co.il"
+*/
+
+--- Exercise Number 11
+
+/*
+Return a table based on the 4 tables shown - the table will display the full name of
+The employees, the names of the departments in which they work, the total amount of sales for each employee, according to
+The name of the product, only for the employees who started their work after 2010
+*/
+
+--- Exercise Number 12
+
+/*
+Write a query showing the last sale made by employee number '555'
+The following fields must be displayed: first name, last name, product name, price, quantity
+units, year of sale and month of sale
+/*
